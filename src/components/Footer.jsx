5d8bccb20c07e12d563cfbc7740ee3bc5ab1129c
lenc <lenc@daihocz.local>
2024-07-05 09:25:55 +0200

    verze2

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+export const Footer = () =>{
+
+return(
+
+    <footer id="footer">
+      <div className="container">
+        <div className="footer__content">
+          Café Lóra je tréningový projekt v rámci Czechitas kurzu JavaScript 2
+        </div>
+      </div>
+    </footer>
+)}
